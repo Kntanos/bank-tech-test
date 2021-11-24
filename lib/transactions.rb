e451bad5b29f92c66d08b9db22616b23e7abcd8a
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-11-24 11:01:06 +0000

    Refactor Transactions to include #balance_update method

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -8,12 +8,16 @@ class Transactions
 
   def self.deposit(date, amount)
     @@list.push({ date: date, amount: amount, new_balance: @@balance + amount })
-    @@balance += amount
+    balance_update(amount)
   end
 
   def self.withdrawal(date, amount)
     @@list.push({ date: date, amount: -amount.abs, new_balance: @@balance - amount.abs })
-    @@balance -= amount.abs
+    balance_update(amount)
+  end
+
+  def self.balance_update(amount)
+    amount.positive? ? @@balance += amount : @@balance -= amount.abs
   end
 
   def self.list
